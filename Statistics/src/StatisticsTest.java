11d1b30c4fb67b07729f5bd91540ee29405857d4
khumps <kmanh999@gmail.com>
2014-11-10 13:19:33 -0500

    commit

--- Statistics/src/StatisticsTest.java
+++ Statistics/src/StatisticsTest.java
@@ -2,12 +2,27 @@
 public class StatisticsTest {
 
 	public static void main(String[] args) {
-		for(double[] data = new double[10];;)
-				for (int i = 0; i < data.length; i++) {
-					data[i] = Math.random() * 100;
+		int i = 0;
+		for(double[] data = new double[10 + i]; i < 50; i += 10, data = new double[10 + i])
+		{
+				for (int j = 0; j < data.length; j++) {
+					data[j] = (int)(Math.random() * 10000) / 100.0;
 				}
+				int zScore = (int)(Math.random() * data.length);
+				System.out.println("Original Data: " + Statistics.arrayToString(data));
+				System.out.println("Sorted Data: " + Statistics.arrayToString(Statistics.sortData(data)));
+				System.out.println("Mean of Data: " + Statistics.mean(data));
+				System.out.println("Median of Data: " + Statistics.median(data));
+				System.out.println("Range of Data: " + Statistics.range(data));
+				System.out.println("Lower Quartile of Data: " + Statistics.lowerQuartile(data));
+				System.out.println("Upper Quartile of Data: " + Statistics.upperQuartile(data));
+				System.out.println("Interquartile Range of Data: " + Statistics.interquartileRange(data));
+				System.out.println("Mode of Data: " + Statistics.mode(data));
+				System.out.println("Standard Deviation of Data: " + Statistics.standardDeviation(data));
+				System.out.println("Z-Score of " + data[zScore] + " in this data set: " + Statistics.zScore(data, zScore));
 				
-
+		}
+		
 	}
 
 }
