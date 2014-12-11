47f4e1bce0f70690396e665673236cf933dc65fa
khumps <kmanh999@gmail.com>
2014-12-10 20:19:27 -0500

    commit

--- Game Of Life 2/src/RulesDialog.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.JDialog;
-import java.awt.event.ActionLister;
-import java.awt.event.ActionEvent;
-
-public class RulesDialog extends JDialog implements ActionListener
-	{
-		JButton 
-	RulesDialog
-}
